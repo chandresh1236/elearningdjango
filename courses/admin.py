82742574be8bba7e8133118d33bcc9380e28848b
chandresh1236 <chandresh6531@gmail.com>
2020-08-14 17:18:38 +0530

    Intial Commit

---
+++ courses/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+admin.site.register(Course)
+admin.site.register(YTLink)
+admin.site.register(FileUpload)
+admin.site.register(TextBlock)
+# Register your models here.
